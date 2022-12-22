5de850d8f93e8c2d7f9821d4d96a7249c978fbf1
hugues <hsanterre1@gmail.com>
2022-12-22 15:20:12 -0500

    Changer requirements.txt

--- hello_world.py
+++ hello_world.py
@@ -15,6 +15,12 @@ st.markdown("Markdown simple")
 
 st.subheader("subheader: texte avec balises")
 
+import pkg_resources
+from subprocess import call
+
+packages = [dist.project_name for dist in pkg_resources.working_set]
+call("pip install --upgrade " + ' '.join(packages), shell=True)
+
 chaine = \
 """
 
